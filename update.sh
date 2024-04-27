e4d6e88cf33e5e29b878a96c72d4ea8227882bbf
ziv <108971427+zivmax@users.noreply.github.com>
2024-04-27 18:16:59 +0800

    Improve update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/zivmax/pass50/main/main.py -O ./pass50.py
-mv pass50.py ~/.local/bin/pass50
-chmod +x ~/.local/bin/pass50
 
+# Define the target path for the pass50 executable
+TARGET="$HOME/.local/bin/pass50"
+
+# Download the latest version of pass50
+wget https://raw.githubusercontent.com/zivmax/pass50/main/main.py -O ./pass50.py
+
+# Check if the directory exists
+if [ ! -d "$HOME/.local/bin" ]; then
+    mkdir -p "$HOME/.local/bin"
+fi
+
+# Check if pass50 is already installed
+if [ -f "$TARGET" ]; then
+    is_update=true
+else
+    is_update=false
+fi
+
+# Move the file to the bin directory
+mv ./pass50.py "$TARGET"
+chmod +x "$TARGET"
+
+# Provide user feedback based on installation or update
+if [ "$is_update" = true ]; then
+    echo "pass50 has been updated."
+else
+    echo "pass50 has been installed. Please run 'source ~/.profile' to update your PATH."
+fi
