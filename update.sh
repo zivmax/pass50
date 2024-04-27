91c8caa4506c79ccc4cab8c9c931a4da3519cfb6
ziv <108971427+zivmax@users.noreply.github.com>
2024-04-27 16:06:39 +0800

    Update the install and update script

--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
-cp ~/cs/self/courses/cs50/pass50/pass50.py ~/.local/bin/pass50
+wget https://raw.githubusercontent.com/zivmax/pass50/main/main.py
+mv main.py ~/.local/bin/pass50
 chmod +x ~/.local/bin/pass50
+
