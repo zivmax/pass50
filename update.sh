81801537938afdbb422dcb77dbc77c1ab0ec0093
ziv <108971427+zivmax@users.noreply.github.com>
2024-04-27 16:13:23 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-wget -P ./pass50.py https://raw.githubusercontent.com/zivmax/pass50/main/main.py
+wget https://raw.githubusercontent.com/zivmax/pass50/main/main.py -O ./pass50.py
 mv pass50.py ~/.local/bin/pass50
 chmod +x ~/.local/bin/pass50
 
