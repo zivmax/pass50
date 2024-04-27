fbca666a52d88ec8ee52b2a17d0ea7926a93e15c
ziv <108971427+zivmax@users.noreply.github.com>
2024-04-27 16:07:35 +0800

    Update the install and update s cript

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/zivmax/pass50/main/main.py
-mv main.py ~/.local/bin/pass50
+wget -P ./pass50.py https://raw.githubusercontent.com/zivmax/pass50/main/main.py
+mv pass50.py ~/.local/bin/pass50
 chmod +x ~/.local/bin/pass50
 
